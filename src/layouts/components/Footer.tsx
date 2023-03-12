38606a9551f7ae99eb848f8396205a3f15af5d02
badkamyk <slothmateusz@gmail.com>
2023-03-12 21:03:32 +0100

    feat: changed Footer spacing and colors

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -2,6 +2,7 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import AdbIcon from '@mui/icons-material/Adb';
 
 const footerLinks = [
   { title: 'Home', href: '/' },
@@ -19,24 +20,31 @@ export default function Footer() {
   return (
     <BottomNavigation
       component={'footer'}
-      sx={{ backgroundColor: 'primary.main', color: 'primary.contrastText', mt: 3, pt: 3, pb: 6, bottom: 0 }}
+      sx={{
+        background: 'linear-gradient(-90deg,#02203c,#001528)',
+        color: 'primary.contrastText',
+        mt: 3,
+        py: 4,
+        bottom: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
     >
-      <Typography variant={'body2'} align={'center'} sx={{ width: '100%' }}>
-        ITBRIDGE © 2023
-      </Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-        {footerLinks.map((link) => (
-          <Link
-            key={link.title}
-            href={link.href}
-            underline="none"
-            sx={{ color: 'primary.contrastText', mx: 2, fontSize: { xs: '0.8rem', sm: '1rem' } }}
-          >
-            {link.title}
-          </Link>
-        ))}
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+      {/*<Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>*/}
+      {/*  {footerLinks.map((link) => (*/}
+      {/*    <Link*/}
+      {/*      key={link.title}*/}
+      {/*      href={link.href}*/}
+      {/*      underline="none"*/}
+      {/*      sx={{ color: 'primary.contrastText', mx: 2, fontSize: { xs: '0.8rem', sm: '1rem' } }}*/}
+      {/*    >*/}
+      {/*      {link.title}*/}
+      {/*    </Link>*/}
+      {/*  ))}*/}
+      {/*</Box>*/}
+      <Box>
         {socialLinks.map((link) => (
           <Link
             key={link.title}
@@ -44,10 +52,13 @@ export default function Footer() {
             underline="none"
             sx={{ color: 'primary.contrastText', mx: 2, fontSize: { xs: '0.8rem', sm: '1rem' } }}
           >
-            {link.title}
+            <AdbIcon />
           </Link>
         ))}
       </Box>
+      <Typography variant={'body2'} align={'center'} sx={{ width: '100%', flexGrow: 1 }}>
+        ITBRIDGE © 2023
+      </Typography>
     </BottomNavigation>
   );
 }
