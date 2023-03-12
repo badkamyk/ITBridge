f8edfe80aca1a6e02b232b8063b19054e7273106
badkamyk <slothmateusz@gmail.com>
2023-03-12 21:13:37 +0100

    feat: added MainCard component

---
+++ src/features/MainContentCard/components/MainCard.tsx
@@ -0,0 +1,55 @@
+import SpeakerNotesOutlinedIcon from '@mui/icons-material/SpeakerNotesOutlined';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+// TODO add props
+export default function MainCard() {
+  return (
+    <Box
+      sx={{
+        minWidth: 275,
+        overflow: 'hidden',
+        borderRadius: 1,
+        boxShadow: 1,
+        m: 1,
+      }}
+    >
+      <Card
+        sx={{
+          '&:hover': {
+            bgcolor: 'grey.100',
+            boxShadow: 3,
+            cursor: 'pointer',
+            maxHeight: 300,
+            transition: 'all 0.2s ease-in-out',
+            '& .card-svg': {
+              transform: 'scale(1.2)',
+            },
+          },
+        }}
+      >
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Box sx={{ maxHeight: 50, mb: 1.5 }}>
+            <SpeakerNotesOutlinedIcon
+              className="card-svg"
+              sx={{ fontSize: 50, transition: 'transform .3s', color: 'primary.main' }}
+            />
+          </Box>
+          <Typography sx={{ mb: 1.5, fontWeight: 'bold' }} color="black" gutterBottom>
+            adjective
+          </Typography>
+          <Typography variant="body2" sx={{ maxWidth: 200, textAlign: 'center' }}>
+            well meaning and kindly. well meaning and kindly. well meaning and kindly. well meaning and kindly.
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ justifyContent: 'center' }}>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+}
