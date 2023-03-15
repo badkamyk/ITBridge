c2167c9417e2ac36769dd540decbcafcbff29709
badkamyk <slothmateusz@gmail.com>
2023-03-15 17:45:26 +0100

    refactor: moved cardInfo to MainContentCardContainer, removed unnecessary props

--- src/features/MainContentCard/containers/MainContentCardContainer.tsx
+++ src/features/MainContentCard/containers/MainContentCardContainer.tsx
@@ -1,6 +1,39 @@
 import Box from '@mui/material/Box';
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import BackgroundSVG from '../components/BackgroundSVG';
+import MainCard from '../components/MainCard';
 
-export default function MainContentCardContainer({ children }: React.PropsWithChildren) {
+const cardInfo = [
+  {
+    title: 'React',
+    description: 'We will start with the basics of React and then move on to more advanced topics',
+    icon: (
+      <JavascriptIcon className="card-svg" sx={{ fontSize: 50, transition: 'transform .3s', color: 'primary.main' }} />
+    ),
+  },
+  {
+    title: 'Angular',
+    description: 'Angular lessons will be taught by a senior developer with 10+ years of experience',
+    icon: (
+      <JavascriptIcon className="card-svg" sx={{ fontSize: 50, transition: 'transform .3s', color: 'primary.main' }} />
+    ),
+  },
+  {
+    title: 'Svelte',
+    description: 'Modern approach to building web applications, with less boilerplate and a smaller learning curve',
+    icon: (
+      <JavascriptIcon className="card-svg" sx={{ fontSize: 50, transition: 'transform .3s', color: 'primary.main' }} />
+    ),
+  },
+  {
+    title: 'Vue',
+    description: 'Lessons are structured in a way that will help you learn Vue in a short period of time',
+    icon: (
+      <JavascriptIcon className="card-svg" sx={{ fontSize: 50, transition: 'transform .3s', color: 'primary.main' }} />
+    ),
+  },
+];
+export default function MainContentCardContainer() {
   return (
     <Box
       sx={{
@@ -12,7 +45,10 @@ export default function MainContentCardContainer({ children }: React.PropsWithCh
         pt: { xs: 2, sm: 8 },
       }}
     >
-      {children}
+      <BackgroundSVG />
+      {cardInfo.map((card, index) => {
+        return <MainCard key={card.title + index} title={card.title} description={card.description} icon={card.icon} />;
+      })}
     </Box>
   );
 }
