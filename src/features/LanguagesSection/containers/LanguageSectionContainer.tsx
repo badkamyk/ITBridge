c3a548e7385d49e189e1bb49ecd988689df601b5
badkamyk <slothmateusz@gmail.com>
2023-03-16 18:49:54 +0100

    feat: added LanguageSectionContainer

---
+++ src/features/LanguagesSection/containers/LanguageSectionContainer.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@mui/material';
+import LanguagesDescription from '../components/LanguagesDescription';
+import Cube from '../components/Cube';
+
+export default function LanguageSectionContainer() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexDirection: { xs: 'column-reverse', md: 'row' },
+        bgcolor: 'grey.100',
+        px: { xs: 2, sm: 12, md: 20, lg: 30 },
+        py: { xs: 2, sm: 6 },
+        textAlign: { xs: 'center', md: 'left' },
+      }}
+    >
+      <LanguagesDescription />
+      <Cube />
+    </Box>
+  );
+}
