417ed31f791127705d4701aec0353031486a31cf
badkamyk <slothmateusz@gmail.com>
2023-03-16 18:51:20 +0100

    feat: changed MentorCard styling

--- src/features/MentorSection/components/MentorCard.tsx
+++ src/features/MentorSection/components/MentorCard.tsx
@@ -2,12 +2,14 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Box } from '@mui/material';
 
 export default function MentorCard({ url, name, description }: { url: string; name: string; description: string }) {
   return (
-    <Card sx={{ maxWidth: 345, borderRadius: 2, boxShadow: 1, m: 1 }}>
-      <CardActionArea>
+    <Card
+      sx={{ minWidth: 145, borderRadius: 2, boxShadow: 1, m: 1, minHeight: { xs: 350, sm: 400, md: 450, lg: 400 } }}
+    >
+      <Box>
         <CardMedia
           sx={{ backgroundPosition: 'center', backgroundSize: 'cover' }}
           component="img"
@@ -23,7 +25,7 @@ export default function MentorCard({ url, name, description }: { url: string; na
             {description}
           </Typography>
         </CardContent>
-      </CardActionArea>
+      </Box>
     </Card>
   );
 }
