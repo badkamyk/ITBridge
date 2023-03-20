f09ee8713781ae616788dbec40176f31078faca8
badkamyk <slothmateusz@gmail.com>
2023-03-20 23:37:02 +0100

    refactor: added separate type for BoldHeader component

--- src/shared/ui/Header/BoldHeader.tsx
+++ src/shared/ui/Header/BoldHeader.tsx
@@ -1,8 +1,14 @@
 import { Typography } from '@mui/material';
 
-export default function BoldHeader3({ text }: { text: string }) {
+export default function BoldHeader({
+  variant,
+  text,
+}: {
+  variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  text: string;
+}) {
   return (
-    <Typography variant="h3" sx={{ fontWeight: 500, mb: 2, mt: 6, textAlign: 'center' }}>
+    <Typography variant={variant} sx={{ fontWeight: 500, mb: 2, mt: 6, textAlign: 'center' }}>
       {text}
     </Typography>
   );
