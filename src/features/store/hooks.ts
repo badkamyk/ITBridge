85d937c4cd2e5f31ffe717ebd2c8853b1c283135
badkamyk <slothmateusz@gmail.com>
2023-03-23 21:57:24 +0100

    feat: added typed useAppDispatch hook

--- src/features/store/hooks.ts
+++ src/features/store/hooks.ts
@@ -1,4 +1,6 @@
-import { RootState } from './store';
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from './store';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
