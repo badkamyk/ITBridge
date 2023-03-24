2923afd77eec38c167d75fe61586ea15f35ee318
badkamyk <slothmateusz@gmail.com>
2023-03-24 22:44:01 +0100

    feat: adding new prop to BoldHeader component

--- src/shared/ui/Header/BoldHeader.tsx
+++ src/shared/ui/Header/BoldHeader.tsx
@@ -3,12 +3,14 @@ import { Typography } from '@mui/material';
 export default function BoldHeader({
   variant,
   text,
+  component = 'h1',
 }: {
   variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   text: string;
+  component?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }) {
   return (
-    <Typography variant={variant} sx={{ fontWeight: 500, mb: 2, mt: 6, textAlign: 'center' }}>
+    <Typography variant={variant} component={component} sx={{ fontWeight: 500, mb: 2, mt: 6, textAlign: 'center' }}>
       {text}
     </Typography>
   );
