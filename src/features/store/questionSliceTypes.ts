bb8f93a9e1bc4d0da6924f108ed639d68674b6ca
badkamyk <slothmateusz@gmail.com>
2023-03-24 22:48:07 +0100

    feat: added answer property

--- src/features/store/questionSliceTypes.ts
+++ src/features/store/questionSliceTypes.ts
@@ -2,6 +2,7 @@ export type QuestionType = {
   id: string;
   question: string;
   date: string;
+  answer?: string;
 };
 
 export type RequireOnly<T, P extends keyof T> = Pick<T, P> & Partial<Omit<T, P>>;
