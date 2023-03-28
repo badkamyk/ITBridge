a8f62ad8db28859bf119db637dc52b130a43b7f8
badkamyk <slothmateusz@gmail.com>
2023-03-28 11:44:38 +0200

    refactor: wrapped ChooseLanguageContainer and AskMentorForm in

--- src/features/AskMentorSection/containers/AskMentorContainer.tsx
+++ src/features/AskMentorSection/containers/AskMentorContainer.tsx
@@ -1,14 +1,12 @@
 import { Container } from '@mui/material';
 import BoldHeader from '../../../shared/ui/Header/BoldHeader';
-import AskMentorForm from '../components/AskMentorForm';
-import ChooseLanguageContainer from './ChooseLanguageContainer';
+import FormContainer from './FormContainer';
 
 export default function AskMentorContainer() {
   return (
     <Container maxWidth="lg">
       <BoldHeader variant={'h4'} text={'Choose language '} />
-      <ChooseLanguageContainer />
-      <AskMentorForm />
+      <FormContainer />
     </Container>
   );
 }
